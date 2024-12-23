63cb7b684141bd971c341f68032156e5c38efd53
Dammymure <dammymure1@gmail.com>
2024-12-23 17:45:56 +0100

    vercel host

--- src/app/admin/events/[id]/edit/page.tsx
+++ src/app/admin/events/[id]/edit/page.tsx
@@ -2,11 +2,29 @@ import db from "@/db/db";
 import { PageHeader } from "../../../_components/PageHeader";
 import { EventForm } from "../../_components/EventForm";
 
+interface PageProps {
+    params: {
+        then: string;
+        catch: string;
+        finally: string;
+        [Symbol.toStringTag]: string;
+        id: string;
+    };
+}
 
-export default async function EditEventPage({ params }: { params: { id: any } }){
-    const event = await db.event.findUnique({ where: { id: params.id } });
-    return( <>
-    <PageHeader>Edit Event</PageHeader>
-    <EventForm event={event}/>
-    </>)
+export default async function EditEventPage({ params }: PageProps) {
+    const { id } = params;
+
+    const event = await db.event.findUnique({ where: { id } });
+
+    if (!event) {
+        return <div>Event not found</div>; // Handle case where event is not found
+    }
+
+    return (
+        <>
+            <PageHeader>Edit Event</PageHeader>
+            <EventForm event={event} />
+        </>
+    );
 }

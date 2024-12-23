afb076e8f8a206f79b7a9d05da45744042114d06
Dammymure <dammymure1@gmail.com>
2024-12-23 18:51:17 +0100

    vercel host

--- src/app/admin/events/[id]/edit/page.tsx
+++ src/app/admin/events/[id]/edit/page.tsx
@@ -1,32 +1,27 @@
-// import db from "@/db/db";
-// import { PageHeader } from "../../../_components/PageHeader";
-// import { EventForm } from "../../_components/EventForm";
+import db from "@/db/db";
+import { PageHeader } from "../../../_components/PageHeader";
+import { EventForm } from "../../_components/EventForm";
 
-// type EditEventPageProps = {
-//     params:{
-//         id: string;
-//     }
-       
-// }
-
-// export default async function EditEventPage({ params }: EditEventPageProps) {
-
-//     const { id } = params;
-//     const event = await db.event.findUnique({ where: { id } });
-
-//     if (!event) {
-//         return <div>Event not found</div>; // Handle case where event is not found
-//     }
-
-//     return (
-//         <>
-//             <PageHeader>Edit Event</PageHeader>
-//             <EventForm event={event} />
-//         </>
-//     );
-// }
-
-
-    export default function EditEventPage(){
-        return <h1>Hello</h1>
+type EditEventPageProps = {
+    params:{
+        id: Promise<any>;
     }
+}
+
+export default async function EditEventPage({ params }: EditEventPageProps) {
+
+    const { id: idPromise } = params;
+    const id = await idPromise;
+    const event = await db.event.findUnique({ where: { id } });
+
+    if (!event) {
+        return <div>Event not found</div>; // Handle case where event is not found
+    }
+
+    return (
+        <>
+            <PageHeader>Edit Event</PageHeader>
+            <EventForm event={event} />
+        </>
+    );
+}

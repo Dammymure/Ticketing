7c4484d6bb1d215ff2e78c4a14f11690a4068e08
Dammymure <dammymure1@gmail.com>
2024-12-23 13:22:45 +0100

    Admin page

--- src/app/admin/events/[id]/edit/page.tsx
+++ src/app/admin/events/[id]/edit/page.tsx
@@ -2,13 +2,16 @@ import { PageHeader } from "../../../_components/PageHeader";
 import { EventForm } from "../../_components/EventForm";
 import db from "@/db/db";
 
-interface EditProductPageProps {
-    params: Promise<{ id: string }>;
-}
+
+// interface EditProductPageProps {
+//     params: Promise<{ id: string }>;
+// }
 
 
-export default async function EditProductPage({ params }: EditProductPageProps) {
-    const event = await db.event.findUnique({ where: { id: params.id } });
+export default async function EditProductPage({params}: {params: Promise<{ id: string }>}) {
+    const { id } = await params;
+
+    const event = await db.event.findUnique({ where: { id } });
 
     if (!event) {
         return <div>Event not found</div>; // Handle case where event is not found

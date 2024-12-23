b930da7fab3b529723da997a7b80c34d38e86aca
Dammymure <dammymure1@gmail.com>
2024-12-23 18:12:05 +0100

    vercel host

--- src/app/admin/events/[id]/edit/page.tsx
+++ src/app/admin/events/[id]/edit/page.tsx
@@ -1,27 +1,27 @@
-import db from "@/db/db";
-import { PageHeader } from "../../../_components/PageHeader";
-import { EventForm } from "../../_components/EventForm";
+// import db from "@/db/db";
+// import { PageHeader } from "../../../_components/PageHeader";
+// import { EventForm } from "../../_components/EventForm";
 
-type EditEventPageProps = {
-    params:{
-        id: string;
-    }
+// type EditEventPageProps = {
+//     params:{
+//         id: string;
+//     }
        
-}
+// }
 
-export default async function EditEventPage({ params }: EditEventPageProps) {
+// export default async function EditEventPage({ params }: EditEventPageProps) {
 
-    const { id } = params;
-    const event = await db.event.findUnique({ where: { id } });
+//     const { id } = params;
+//     const event = await db.event.findUnique({ where: { id } });
 
-    if (!event) {
-        return <div>Event not found</div>; // Handle case where event is not found
-    }
+//     if (!event) {
+//         return <div>Event not found</div>; // Handle case where event is not found
+//     }
 
-    return (
-        <>
-            <PageHeader>Edit Event</PageHeader>
-            <EventForm event={event} />
-        </>
-    );
-}
+//     return (
+//         <>
+//             <PageHeader>Edit Event</PageHeader>
+//             <EventForm event={event} />
+//         </>
+//     );
+// }

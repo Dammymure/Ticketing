85f26fd18defc7d846671d23a45a9ce2abc8a359
Dammymure <dammymure1@gmail.com>
2024-12-23 13:51:59 +0100

    vercel host

--- src/app/admin/events/[id]/edit/page.tsx
+++ src/app/admin/events/[id]/edit/page.tsx
@@ -2,20 +2,38 @@ import { PageHeader } from "../../../_components/PageHeader";
 import { EventForm } from "../../_components/EventForm";
 import db from "@/db/db";
 
+// interface PageProps {
+//     params: Promise<{ id: string }> & {
+//         then: Promise<{ id: string }>['then'];
+//         catch: Promise<{ id: string }>['catch'];
+//         finally: Promise<{ id: string }>['finally'];
+//         [Symbol.toStringTag]: string;
+//     };
+// }
+
 interface PageProps {
-    params: Promise<{ id: string }> & {
-        then: Promise<{ id: string }>['then'];
-        catch: Promise<{ id: string }>['catch'];
-        finally: Promise<{ id: string }>['finally'];
-        [Symbol.toStringTag]: string;
+    params: {
+        id: string;
     };
 }
 
-export default async function EditProductPage({ params }: PageProps) {
-    const resolvedParams = await params; // Await the params if it's a Promise
-    const { id } = resolvedParams;
+interface Event {
+    id: string;
+    title: string;
+    date: Date;
+    pricePaidInCents: number;
+    clientId: string;
+    isAvailableForPurchase: boolean;
+    NumberOfTickets: number;
+    imagePath: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
-    const event = await db.event.findUnique({ where: { id } });
+export default async function EditProductPage({ params }: PageProps) {
+    const { id } = params;
+    const event: Event | null = await db.event.findUnique({ where: { id } });
 
     if (!event) {
         return <div>Event not found</div>; // Handle case where event is not found
